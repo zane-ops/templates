480c7e7b4b9c9772920b4d6a712d0945d7cb233f
Fred KISSIE <fredkiss3@gmail.com>
2026-02-19 19:58:17 +0100

    💄 starting to work on the homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,9 @@ export default defineConfig({
   adapter: node({
     mode: "standalone"
   }),
+  devToolbar: {
+    enabled: false
+  },
   integrations: [
     starlight({
       title: "ZaneOps documentation",
