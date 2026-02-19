2edc913f2b6cc369d743c94e1f04393eb936e67e
Fred KISSIE <fredkiss3@gmail.com>
2026-02-19 19:47:23 +0100

    🚧 prepare env for work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,59 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+
+import node from "@astrojs/node";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  output: "static",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [
+    starlight({
+      title: "ZaneOps documentation",
+      logo: {
+        light: "./src/assets/ZaneOps-SYMBOL-BLACK.svg",
+        dark: "./src/assets/ZaneOps-SYMBOL-WHITE.svg",
+        replacesTitle: true
+      },
+      editLink: {
+        baseUrl: "https://github.com/zane-ops/templates/edit/main/"
+      },
+      customCss: [
+        "./src/assets/global.css",
+        "./src/assets/fonts/font-face.css"
+      ],
+      social: [
+        {
+          label: "Github",
+          icon: "github",
+          href: "https://github.com/zane-ops/zane-ops"
+        },
+        {
+          label: "Discord",
+          icon: "discord",
+          href: "https://zaneops.dev/discord"
+        }
+      ],
+      components: {
+        Footer: "./src/components/Footer.astro",
+        Head: "./src/components/Head.astro"
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" }
+          ]
+        }
+      ]
+    })
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
